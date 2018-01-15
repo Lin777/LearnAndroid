366778ced9bf4398c080beed70708e0b8a3a7755
lin777 <cusi.evelyn@gmail.com>
2018-01-15 01:20:21 -0400

    Aplicacion Contador masparte logica

--- Contador/app/src/main/java/com/example/lin777/contador/MainActivity.java
+++ Contador/app/src/main/java/com/example/lin777/contador/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.lin777.contador;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+
+    public int contador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        contador = 0;
+
     }
 
+    public void incrementaContador(View vista){
+        contador++;
+        mostrarResultados();
+    }
+
+    public void restaContador(View vista){
+        contador--;
+        mostrarResultados();
+    }
+
+    public void reseteaContador(View vista){
+        contador = 0;
+        mostrarResultados();
+    }
+
+    public void mostrarResultados(){
+        TextView textoResultado = (TextView)findViewById(R.id.contadorPulsaciones);
+        textoResultado.setText("Contador: "+contador);
+    }
 }
