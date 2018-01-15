ff84bb25e50c3a835221d90da09ca6a04052e134
lin777 <cusi.evelyn@gmail.com>
2018-01-15 15:56:05 -0400

    Contador, agregando logica en el mainActivity

--- Contador/app/src/main/java/com/example/lin777/contador/MainActivity.java
+++ Contador/app/src/main/java/com/example/lin777/contador/MainActivity.java
@@ -3,37 +3,51 @@ package com.example.lin777.contador;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     public int contador;
 
+    TextView textoResultado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        textoResultado = (TextView)findViewById(R.id.contador);
         contador = 0;
     }
 
     public void incrementaContador(View vista){
         contador++;
-        mostrarResultados();
+        textoResultado.setText(""+contador);
     }
 
     public void restaContador(View vista){
+
         contador--;
-        mostrarResultados();
+
+        if(contador<0){
+            CheckBox negativos = (CheckBox)findViewById(R.id.negativos);
+            if(!negativos.isChecked()) {
+                contador = 0;
+            }
+        }
+        textoResultado.setText(""+contador);
     }
 
     public void reseteaContador(View vista){
-        contador = 0;
-        mostrarResultados();
-    }
-
-    public void mostrarResultados(){
-        TextView textoResultado = (TextView)findViewById(R.id.contador);
+        EditText numeroReset = (EditText)findViewById(R.id.n_reseteo);
+        try {
+            contador = Integer.parseInt(numeroReset.getText().toString());
+        }catch (Exception e){
+            contador=0;
+        }
+        numeroReset.setText("");
         textoResultado.setText(""+contador);
     }
+
 }
