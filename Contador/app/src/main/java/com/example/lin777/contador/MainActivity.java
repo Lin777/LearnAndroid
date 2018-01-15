1ac2855f4ac616b894fb4f992f3bf2eb9480ef32
lin777 <cusi.evelyn@gmail.com>
2018-01-15 17:02:11 -0400

    Contador, ocultar el teclado automaticamente al presionar resetear

--- Contador/app/src/main/java/com/example/lin777/contador/MainActivity.java
+++ Contador/app/src/main/java/com/example/lin777/contador/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.lin777.contador;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -48,6 +49,9 @@ public class MainActivity extends Activity {
         }
         numeroReset.setText("");
         textoResultado.setText(""+contador);
+
+        InputMethodManager miTeclado = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+        miTeclado.hideSoftInputFromWindow(numeroReset.getWindowToken(),0);
     }
 
 }
