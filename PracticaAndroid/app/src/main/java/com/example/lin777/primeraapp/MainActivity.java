af2743bce2c71d86116abc0093a5e860889a5452
lin777 <cusi.evelyn@gmail.com>
2018-01-16 15:00:31 -0400

    Aprendiendo constraintLayout, errores, revisar

--- PracticaAndroid/app/src/main/java/com/example/lin777/primeraapp/MainActivity.java
+++ PracticaAndroid/app/src/main/java/com/example/lin777/primeraapp/MainActivity.java
@@ -16,20 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });/*
-        TextView mitexto = new TextView(this);
-        mitexto.setText("Hola mundo");
-        setContentView(mitexto);*/
     }
 
     @Override
