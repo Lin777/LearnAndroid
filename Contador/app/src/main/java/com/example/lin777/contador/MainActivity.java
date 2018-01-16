69b0d13e1b9690ad4f056f525d6d6982254633c6
lin777 <cusi.evelyn@gmail.com>
2018-01-16 17:07:46 -0400

    Eventos teclado desde JAVA ocultando telcado lanzar eventos

--- Contador/app/src/main/java/com/example/lin777/contador/MainActivity.java
+++ Contador/app/src/main/java/com/example/lin777/contador/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.lin777.contador;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -20,6 +22,23 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         textoResultado = (TextView)findViewById(R.id.contador);
         contador = 0;
+
+        //Teclado a la escucha
+        EventoTeclado teclado = new EventoTeclado();
+        EditText nReseteo = (EditText)findViewById(R.id.n_reseteo);
+
+        nReseteo.setOnEditorActionListener(teclado);
+    }
+
+    class EventoTeclado implements TextView.OnEditorActionListener{
+
+        @Override
+        public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+            if(i == EditorInfo.IME_ACTION_DONE){
+                reseteaContador(null);
+            }
+            return false;
+        }
     }
 
     public void incrementaContador(View vista){
@@ -41,6 +60,7 @@ public class MainActivity extends Activity {
     }
 
     public void reseteaContador(View vista){
+
         EditText numeroReset = (EditText)findViewById(R.id.n_reseteo);
         try {
             contador = Integer.parseInt(numeroReset.getText().toString());
@@ -53,5 +73,4 @@ public class MainActivity extends Activity {
         InputMethodManager miTeclado = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
         miTeclado.hideSoftInputFromWindow(numeroReset.getWindowToken(),0);
     }
-
 }
