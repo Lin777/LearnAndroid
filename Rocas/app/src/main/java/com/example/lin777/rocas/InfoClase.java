c0dfee660f3028f202a433f080ab58bb72eb1094
lin777 <cusi.evelyn@gmail.com>
2018-01-17 10:24:05 -0400

    Agregando una nueva Actividad al proyecto Rocas

---
+++ Rocas/app/src/main/java/com/example/lin777/rocas/InfoClase.java
@@ -0,0 +1,17 @@
+package com.example.lin777.rocas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by lin777 on 1/17/18.
+ */
+
+public class InfoClase extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+    }
+}
--- Rocas/app/src/main/java/com/example/lin777/rocas/MainActivity.java
+++ Rocas/app/src/main/java/com/example/lin777/rocas/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.lin777.rocas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void ejecutarInfo(View vista){
+        Intent i = new Intent(this, InfoClase.class);
+        startActivity(i);
+    }
+
 }
