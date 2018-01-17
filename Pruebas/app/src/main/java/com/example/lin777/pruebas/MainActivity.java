c6f47511ecdff66ca22f61fee2bc8ef26377699b
lin777 <cusi.evelyn@gmail.com>
2018-01-17 09:40:11 -0400

    Acceso a recursos graficos del sistema

--- Pruebas/app/src/main/java/com/example/lin777/pruebas/MainActivity.java
+++ Pruebas/app/src/main/java/com/example/lin777/pruebas/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.lin777.pruebas;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView img = (ImageView)findViewById(R.id.icono_edicion);
+
+        img.setImageResource(android.R.drawable.ic_menu_edit);
     }
 }
